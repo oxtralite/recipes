8da3eae454b02a3a201c1857fa5a5608a80e7273
Benjamin Call <llacneb@gmail.com>
2016-08-19 14:08:34 -0600

    Update ingredients and add new recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,9 +7,17 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
+  def create
+    name = params["recipe"]["name"]
+    @recipe = Recipe.create!(name: name, short_description: "")
+    redirect_to recipes_url
+  end
+
   def update
     recipe = Recipe.find(params[:id])
     recipe.update(params['recipe'].permit(:name, :short_description))
+    ingredient = Ingredient.create!(name: params["recipe"]["ingredients"])
+    recipe.recipe_ingredients.create!(ingredient: ingredient)
     redirect_to recipes_url
   end
 
